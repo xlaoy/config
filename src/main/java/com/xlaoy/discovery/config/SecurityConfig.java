4557a70d73433e65f074c971260d8d1876c65171
xlaoy <768881412@qq.com>
2018-04-12 14:49:55 +0800

    升级2.0版本

--- src/main/java/com/xlaoy/discovery/config/SecurityConfig.java
+++ src/main/java/com/xlaoy/discovery/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers(
-            "/application/**",
+            "/actuator/**",
             "/error",
             "/favicon.ico"
         );
