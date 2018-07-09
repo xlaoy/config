d4a0abf17252b494ca5491b6dff1532124731dfb
xlaoy <768881412@qq.com>
2018-07-09 11:46:13 +0800

    redis

--- src/main/java/com/xlaoy/config/ConfigApplication.java
+++ src/main/java/com/xlaoy/config/ConfigApplication.java
@@ -3,10 +3,8 @@ package com.xlaoy.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
-@EnableEurekaClient
 @SpringCloudApplication
 public class ConfigApplication {
 
